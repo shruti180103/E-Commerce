2e5f8fc359366ee56104c08dbbf84f2043cad7c3
abhinendra2904 <abhinendra.yadav21@st.niituniversity.in>
2024-03-20 23:37:23 +0530

    login page

--- frontend/geargleams/src/components/Login/Login.jsx
+++ frontend/geargleams/src/components/Login/Login.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import {React,useState} from "react";
 
 const Login = () => {
+  const [email,setEmail] = useState("");
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+      <div className="flex justify-center items-center h-screen">
         <h2 className="mt-6 text-colour text-3xl font-extrabold text-gray-900">
           Login to your account
         </h2>
@@ -26,6 +27,7 @@ const Login = () => {
                   required
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                 />
               </div>
             </div>
