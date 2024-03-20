7f55ba83222269721eea8a42714525405049151d
shruti180103 <shruti.agarwal21@st.niituniversity.in>
2024-03-21 00:58:31 +0530

    pass created

--- frontend/geargleams/src/components/Login/Login.jsx
+++ frontend/geargleams/src/components/Login/Login.jsx
@@ -1,12 +1,14 @@
-import {React,useState} from "react";
+import { React, useState } from "react";
 
 const Login = () => {
-  const [email,setEmail] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [visible, setVisible] = useState(false);
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="flex justify-center items-center h-screen">
         <h2 className="mt-6 text-colour text-3xl font-extrabold text-gray-900">
-          Login to your account
+          Login to your Account
         </h2>
       </div>
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md:">
@@ -17,7 +19,7 @@ const Login = () => {
                 htmlFor="email"
                 classname="block text-sm font-medium text-gray-700"
               >
-                Email address
+                Email Address
               </label>
               <div className="mt-1">
                 <input
@@ -31,6 +33,25 @@ const Login = () => {
                 />
               </div>
             </div>
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Password
+              </label>
+              <div className="mt-1 relative">
+                <input
+                  type={visible ? "text" : "password"}
+                  name="password"
+                  autoComplete="current-password"
+                  required
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                />
+              </div>
+            </div>
           </form>
         </div>
       </div>
@@ -39,3 +60,4 @@ const Login = () => {
 };
 
 export default Login;
+
