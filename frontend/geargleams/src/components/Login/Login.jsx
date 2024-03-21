0726cc8ad39dcfbb09e29d7b8de110d5cf9b2498
shruti180103 <shruti.agarwal21@st.niituniversity.in>
2024-03-21 22:27:28 +0530

    remember me

--- frontend/geargleams/src/components/Login/Login.jsx
+++ frontend/geargleams/src/components/Login/Login.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import styles from "../../styles/styles";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -40,7 +42,7 @@ const Login = () => {
               >
                 Password
               </label>
-              <div className="mt-1 relativ">
+              <div className="mt-1 relative">
                 <input
                   type={visible ? "text" : "password"}
                   name="password"
@@ -50,6 +52,35 @@ const Login = () => {
                   onChange={(e) => setPassword(e.target.value)}
                   className="appearance-none block w-full sm:w-72 px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                 />
+                {visible ? (
+                  <AiOutlineEye
+                    className="absolute right-2 top-2 cursor-pointer"
+                    size={25}
+                    onClick={() => setVisible(false)}
+                  />
+                ) : (
+                  <AiOutlineEyeInvisible
+                    className="absolute right-2 top-2 cursor-pointer"
+                    size={25}
+                    onClick={() => setVisible(true)}
+                  />
+                )}
+              </div>
+            </div>
+            <div className={`${styles.noramlFlex} justify-between`}>
+              <div className={`${styles.noramlFlex}`}>
+                <input
+                  type="checkbox"
+                  name="remember-me"
+                  id="remember-me"
+                  className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
+                />
+                <label
+                  htmlFor="remember-me"
+                  className="ml-2 block text-sm text-gray-900"
+                >
+                  Remember me
+                </label>
               </div>
             </div>
           </form>
