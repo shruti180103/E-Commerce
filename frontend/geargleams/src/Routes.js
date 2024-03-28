b444980a65870c72eaf37f77389646b95bca61d4
abhinendra2904 <abhinendra.yadav21@st.niituniversity.in>
2024-03-28 10:26:46 +0530

    signup page build

--- frontend/geargleams/src/Routes.js
+++ frontend/geargleams/src/Routes.js
@@ -1,6 +1,7 @@
-import LoginPage from "../src/pages/Login.jsx";
-
+import LoginPage from "../src/pages/Login";
+import SignupPage from "../src/pages/SignupPage";
 
 export{
     LoginPage,
+    SignupPage,
 };
---
+++ frontend/geargleams/src/pages/SignupPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Signup from ".../component/Signup/Signup.jsx";
+
+const SignupPage = () => {
+    <div>
+        <Signup/>
+    
+    </div>
+}
+export default SignupPage
