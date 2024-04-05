af36e3e4fe73744f71351b9db7018e475cd6999c
shruti180103 <shruti.agarwal21@st.niituniversity.in>
2024-04-05 17:21:01 +0530

    signup page connected

--- frontend/geargleams/src/components/Signup/Signup.jsx
+++ frontend/geargleams/src/components/Signup/Signup.jsx
@@ -2,29 +2,39 @@ import { React, useState } from "react";
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import styles from "../../styles/styles";
 import { Link } from "react-router-dom";
-import axios from "axios";
+import { RxAvatar } from "react-icons/rx";
+import  axios  from "axios";
 import { server } from "../../server";
-import { toast } from "react-toastify";
 
 const Singup = () => {
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const [visible, setVisible] = useState(false);
+  const [avatar, setAvatar] = useState(null);
+
+  const handleFileInputChange = (e) => {
+    const file = e.target.files[0];
+    setAvatar(file);
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    const config = { Headers: { "Content-Type": "multipart/form-data" } };
 
+    const newForm = new FormData();
+
+    newForm.append("file", avatar);
+    newForm.append("username", name);
+    newForm.append("email", email);
+    newForm.append("password", password);
     axios
-      .post(`${server}/user/create-user`, { name, email, password })
+      .post(`${server}/user/create-user`, newForm, config)
       .then((res) => {
-        toast.success(res.data.message);
-        setName("");
-        setEmail("");
-        setPassword("");
+        console.log(res);
       })
-      .catch((error) => {
-        toast.error(error.response.data.message);
+      .catch((err) => {
+        console.log(err);
       });
   };
 
@@ -32,7 +42,7 @@ const Singup = () => {
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-          Register as a New User
+          Register as a new user
         </h2>
       </div>
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
@@ -112,6 +122,40 @@ const Singup = () => {
             </div>
 
             <div>
+              <label
+                htmlFor="avatar"
+                className="block text-sm font-medium text-gray-700"
+              ></label>
+              <div className="mt-2 flex items-center">
+                <span className="inline-block h-8 w-8 rounded-full overflow-hidden">
+                  {avatar ? (
+                    <img
+                      src={avatar}
+                      alt="avatar"
+                      className="h-full w-full object-cover rounded-full"
+                    />
+                  ) : (
+                    <RxAvatar className="h-8 w-8" />
+                  )}
+                </span>
+                <label
+                  htmlFor="file-input"
+                  className="ml-5 flex items-center justify-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50"
+                >
+                  <span>Upload a file</span>
+                  <input
+                    type="file"
+                    name="avatar"
+                    id="file-input"
+                    accept=".jpg,.jpeg,.png"
+                    onChange={handleFileInputChange}
+                    className="sr-only"
+                  />
+                </label>
+              </div>
+            </div>
+
+            <div>
               <button
                 type="submit"
                 className="group relative w-full h-[40px] flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700"
@@ -120,7 +164,7 @@ const Singup = () => {
               </button>
             </div>
             <div className={`${styles.noramlFlex} w-full`}>
-              <h4>Already have an Account?</h4>
+              <h4>Already have an account?</h4>
               <Link to="/login" className="text-blue-600 pl-2">
                 Sign In
               </Link>
