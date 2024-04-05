d156b2482f2f7b62648d58b6b34af9857482b628
shruti180103 <shruti.agarwal21@st.niituniversity.in>
2024-04-05 23:11:57 +0530

    db code change

--- backend/db/Database.js
+++ backend/db/Database.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 
 const connectDatabase = () => {
   mongoose
-    .connect(process.env.DB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+    .connect(process.env.DB_URL)
+    .then(() => {
+      console.log(`mongodb connected with server: ${mongoose.connection.host}`);
     })
-    .then((data) => {
-      console.log(`mongodb connected with server: $(data.connection.host)`);
+    .catch((error) => {
+      console.error("MongoDB connection error:", error);
     });
 };
 
