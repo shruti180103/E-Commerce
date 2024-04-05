4d6e13698a99011d8703ae1684cf87686c801329
abhinendra2904 <abhinendra.yadav21@st.niituniversity.in>
2024-04-05 15:14:23 +0530

    express changes

--- backend/App.js
+++ backend/App.js
@@ -3,12 +3,12 @@ const ErrorHandler = require("./utils/ErrorHandler");
 const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
-const fileUpload = require( "express-fileupload");
+
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(fileUpload({useTempFiles: true}))
+
 
 
 // config
