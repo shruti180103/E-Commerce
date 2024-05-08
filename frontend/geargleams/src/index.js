50bdf377583568a01c0cd702b9207173df87d9b1
shruti180103 <shruti.agarwal21@st.niituniversity.in>
2024-05-09 03:25:11 +0530

    Reapply "complete project"
    This reverts commit 51f7f74a7dabdea2c33c9f6c806f042c550ff6d1.

--- frontend/geargleams/src/index.js
+++ frontend/geargleams/src/index.js
@@ -1,16 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client"; // Update import statement
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./App.css";
 import { Provider } from "react-redux";
 import Store from "./redux/store";
 
-ReactDOM.render(
-  <Provider store={Store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
+// Replace ReactDOM.render with createRoot
+const root = createRoot(document.getElementById("root"));
+
+// Render your application inside the root
+root.render(
+  <React.StrictMode>
+    <Provider store={Store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
 
 reportWebVitals();
---
+++ frontend/geargleams/src/routes/ShopRoutes.js
@@ -0,0 +1,34 @@
+// Import named exports from ShopRoutes.js
+import {
+    ShopDashboardPage,
+    ShopCreateProduct,
+    ShopAllProducts,
+    ShopCreateEvents,
+    ShopAllEvents,
+    ShopAllCoupouns,
+    ShopPreviewPage,
+    ShopAllOrders,
+    ShopOrderDetails,
+    ShopAllRefunds,
+    ShopSettingsPage,
+    ShopWithDrawMoneyPage,
+    ShopInboxPage
+  } from "../routes/ShopRoutes";
+  
+  // Export the named imports
+  export {
+    ShopDashboardPage,
+    ShopCreateProduct,
+    ShopAllProducts,
+    ShopCreateEvents,
+    ShopAllEvents,
+    ShopAllCoupouns,
+    ShopPreviewPage,
+    ShopAllOrders,
+    ShopOrderDetails,
+    ShopAllRefunds,
+    ShopSettingsPage,
+    ShopWithDrawMoneyPage,
+    ShopInboxPage
+  };
+  
